73be9c79ce8669ffa91503f069092cc2c7972621
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-06-19 16:54:05 +0200

    Added pages and updated nav acordingly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Nav from "./nav/Nav.js";
+import Home from "./pages/Home.js";
+import Projects from "./pages/Projects.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
 
 import "./styles/app.css";
 import "./styles/root.css";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Nav />
-    </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/projects:" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
