a76281414314e5a8693839d4b7c449f7a1464ab9
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-08-19 14:44:08 +0200

    Started working on projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import SelectProject from "../components/showcaseProjects/SelectProject";
-
 import "../styles/projects.css";
 
-const ShowProject = ({ project }) => {
-  return <section className="project-wrapper"> {project} </section>;
+const NoData = () => {
+  return <section>:(</section>;
+};
+
+const ShowProject = ({ filename }) => {
+  const [fileData, setFileData] = useState(null);
+
+  useEffect(() => {
+    const fetchProjectData = async () => {
+      try {
+        const module = await import(
+          `../assets/data/projectData/${filename}.json`
+        );
+        const newFileData = module.default;
+        setFileData(newFileData);
+      } catch (error) {
+        console.error("Error importing file:", error);
+      }
+    };
+
+    fetchProjectData();
+  }, [filename]);
+
+  return (
+    <section className="project-wrapper">
+      {fileData ? <div>{fileData.filename}</div> : <NoData />}
+    </section>
+  );
 };
 
 function Projects() {
@@ -14,7 +39,7 @@ function Projects() {
 
   return (
     <section>
-      {project ? <ShowProject project={project} /> : <SelectProject />}
+      {project ? <ShowProject filename={project} /> : <SelectProject />}
     </section>
   );
 }
