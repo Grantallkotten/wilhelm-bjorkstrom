29f7d1d53d7a1f95e385ff99e47125d4bdf28c40
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2024-09-06 16:58:31 +0200

    Checkpoint

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function About() {
-  return <section>About</section>;
+  return (
+    <section className="about-wrapper">
+      <section className="about-banner" />
+      <section className="about-content-wrapper">
+        <section className="about-content"></section>
+      </section>
+    </section>
+  );
 }
 
 export default About;
